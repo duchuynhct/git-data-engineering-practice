282b88b0024fb5c4a194672f90da841b719e2e09
Huynh Cao Trung Duc <huynhcaotrungduc@gmail.com>
2026-08-18 21:09:42 +0700

    Add user extraction

---
+++ src/extract.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def extract_users(url):
+    response = requests.get(url, timeout=5)
+    response.raise_for_status()
+
+    return response.json()
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users"
+
+    users = extract_users(url)
+
+    print(f"Extracted {len(users)} users")
